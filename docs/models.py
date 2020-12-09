f0da8a610387283fa9fe11d522c34ecd0f97bdb0
su <admin>
2020-12-09 07:29:11 +0200

    docs model migration

--- docs/models.py
+++ docs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Doc(models.Model):
+    doc_name = models.CharField(max_length=255)
+    doc_template = models.CharField(max_length=255)
+    doc_image = models.ImageField(upload_to='docs')
+    doc_price = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.doc_name
+
